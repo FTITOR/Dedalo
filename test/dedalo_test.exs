f0e39a68b04436d338cd1a2f10e8710baa67c72f
zgbjgg <zgbjgg@gmail.com>
2026-04-01 16:16:10 -0600

    update tests

--- test/dedalo_test.exs
+++ test/dedalo_test.exs
@@ -1,8 +1,31 @@
 defmodule DedaloTest do
-  use ExUnit.Case
-  doctest Dedalo
+  @moduledoc false
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert Dedalo.hello() == :world
+  test "LLM not found" do
+    Mix.shell(Mix.Shell.Process)
+  
+    Mix.Tasks.Dedalo.run(["cursor", "--path", ".agents/conf.yml"])
+
+    assert_received {:mix_shell, :error, ["⚠️  no supported LLM, please execute mix help dedalo for more info."]}
+  end
+
+  test "YML not found" do
+    Mix.shell(Mix.Shell.Process)
+
+    Mix.Tasks.Dedalo.run(["claude", "--path", ".agents/conf0.yml"])
+
+    assert_received {:mix_shell, :error, ["⚠️  missing conf file, please ensure file exists on the provided path."]}
+  end
+
+  test "Claude success" do
+    Mix.shell(Mix.Shell.Process)
+
+    Mix.Tasks.Dedalo.run(["claude", "--path", ".agents/conf.yml"])
+
+    assert_received {:mix_shell, :info, ["📖 conf file found, processing ..."]}
+    assert_received {:mix_shell, :info, ["✓ Building, linking and syncing with Claude ..."]}
+    assert_received {:mix_shell, :info, [output]}
+    assert output =~ "Available at .claude/settings.json"
   end
 end
