ec2c64854ef66ce1d4a275aba1bb1dc0946a139b
zgbjgg <zgbjgg@gmail.com>
2026-04-07 14:13:40 -0600

    avoid remove internal files used by llm config and not managed by dedalo

--- lib/gemini.ex
+++ lib/gemini.ex
@@ -20,7 +20,7 @@ defmodule Dedalo.Gemini do
   ```
   """
   def build_and_link(opts) do
-    :ok = Enum.each([:rm_rf!, :mkdir_p!], &apply(File, &1, [@base_dir]))
+    :ok = File.mkdir_p!(@base_dir)
 
     deny_conf = Keyword.get(opts, :deny, %{})
     allow_conf = Keyword.get(opts, :allow, %{})
